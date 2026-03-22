cf90f5fcdc3ec74d73aa10282b421148f5fc7e88
uday-venkatesha <udayvenkatesh2015@gmail.com>
2026-03-22 03:29:56 -0500

    feat: Implement incremental loading for bronze bookings and add a new analysis demonstrating Jinja loops for column selection.

---
+++ airbnb_dbt_snowflake_project/analyses/loops.sql
@@ -0,0 +1,8 @@
+{% set cols=['NIGHTS_BOOKED','BOOKING_ID','BOOKING_AMOUNT']%}
+select
+{% for col in cols %}
+    {{col}}
+        {% if not loop.last %},{% endif%}
+
+{% endfor %}
+from {{ref('bronze_bookings')}}
