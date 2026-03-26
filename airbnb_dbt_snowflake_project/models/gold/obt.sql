f621a0019504da40d8d6ab9547ebf1cc1cf18c6c
uday-venkatesha <udayvenkatesh2015@gmail.com>
2026-03-25 22:58:45 -0500

    Project Almost  done

--- airbnb_dbt_snowflake_project/models/gold/obt.sql
+++ airbnb_dbt_snowflake_project/models/gold/obt.sql
@@ -11,9 +11,23 @@
         "join_condition" : "SILVER_bookings.listing_id = SILVER_listings.listing_id"
     },
     {
-        "table" : "AIRBNB.SILVER.SILVER_HOSTS",
-        "columns" : "SILVER_hosts.HOST_NAME, SILVER_hosts.HOST_SINCE, SILVER_hosts.IS_SUPERHOST, SILVER_hosts.RESPONSE_RATE, SILVER_hosts.RESPONSE_RATE_QUALITY, SILVER_hosts.CREATED_AT AS HOST_CREATED_AT",
-        "alias" : "SILVER_hosts",
-        "join_condition" : "SILVER_listings.host_id = SILVER_hosts.host_id"
+        "table" : "AIRBNB.SILVER.SILVER_HOST",
+        "columns" : "SILVER_host.HOST_NAME, SILVER_host.HOST_SINCE, SILVER_host.IS_SUPERHOST, SILVER_host.RESPONSE_RATE, SILVER_host.RESPONSE_RATE_QUALITY, SILVER_host.CREATED_AT AS HOST_CREATED_AT",
+        "alias" : "SILVER_host",
+        "join_condition" : "SILVER_listings.host_id = SILVER_host.host_id"
     }
 ] %}
+
+SELECT 
+    {% for config in congigs %}
+        {{ config['columns'] }}{% if not loop.last %},{% endif %}
+    {% endfor %}
+FROM
+    {% for config in congigs %}
+    {% if loop.first %}
+      {{ config['table'] }} AS {{ config['alias'] }}
+    {% else %}
+        LEFT JOIN {{ config['table'] }} AS {{ config['alias'] }}
+        ON {{ config['join_condition'] }}
+        {% endif %}
+        {% endfor %}
